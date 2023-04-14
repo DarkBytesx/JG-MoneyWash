b965a85c7e9c05e381d3b821445126b6ff57824e
JGUsman <58899378+JGUsman007@users.noreply.github.com>
2023-04-14 11:58:48 +0500

    Add files via upload

---
+++ JG-MoneyWash/shared.lua
@@ -0,0 +1,24 @@
+Config = {}
+
+Config.percentage = math.random(20,30) -----Percentage to cut from the ammount while money wash
+Config.policejob = 'police'
+Config.laundrycarditemname = 'moneywashcard'
+
+Config.zones = {
+    [0] = {
+        coords = vec3(1136.3601, -992.1198, 46.1132),
+        heading = 101.8245
+    },
+    [1] = {
+        coords = vec3(1136.1068, -990.8231, 46.1132),
+        heading = 100.5675
+    },
+    [2] = {
+        coords = vec3(1135.9194, -989.4544, 46.1132),
+        heading = 97.5669
+    },
+    [3] = {
+        coords = vec3(1135.7466, -988.0999, 46.1132),
+        heading = 96.1464
+    }
+}
